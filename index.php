2323336ce37ce25ba24df6ed561ca9ad0911edc9
Andrew Farquharson <farquharson.a@gmail.com>
2019-09-30 00:36:28 +0100

    create tables

--- index.php
+++ index.php
@@ -3,10 +3,24 @@
 require 'vendor/autoload.php';
 
 use PostgreSQLTutorial\Connection as Connection;
+use PostgreSQLTutorial\PostgreSQLCreateTable as PostgreSQLCreateTable;
 
 try {
-	Connection::get()->connect();
-	echo 'A connection to the PostgreSQL database server has been established successfully.';
+    // connect to the PostgreSQL database
+	$pdo = Connection::get()->connect();
+
+	//
+    $tableCreator = new PostgreSQLCreateTable($pdo);
+
+    // create tables and query the table from the
+    // database
+    $tables = $tableCreator->createTables()
+                            ->getTables();
+
+    foreach ($tables as $table) {
+        echo $table . '<br>';
+    }
+
 } catch (\PDOException $e) {
 	echo $e->getMessage();
 }
